038ce71e28e86c0af71b7665bf9bfb6f3ba85427
Ali Raza <aliraza@bu.edu>
2020-06-29 15:19:10 -0400

    changing SYSCALL_DEFINE macros so that we can make direct calls to ukl functions instead of syscalls.

--- arch/x86/include/asm/syscall_wrapper.h
+++ arch/x86/include/asm/syscall_wrapper.h
@@ -224,6 +224,11 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
 
 #endif /* CONFIG_COMPAT */
 
+#ifdef CONFIG_UNIKERNEL_LINUX
+#define __SYSCALL_DEFINEx(x, name, ...)					\
+	static inline long __ukl_##name(__MAP(x,__SC_DECL,__VA_ARGS__));\
+	static inline long __ukl_##name(__MAP(x,__SC_DECL,__VA_ARGS__))
+#else
 #define __SYSCALL_DEFINEx(x, name, ...)					\
 	static long __se_sys##name(__MAP(x,__SC_LONG,__VA_ARGS__));	\
 	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__));\
@@ -237,6 +242,7 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
 		return ret;						\
 	}								\
 	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__))
+#endif
 
 /*
  * As the generic SYSCALL_DEFINE0() macro does not decode any parameters for
@@ -245,12 +251,18 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
  * SYSCALL_DEFINEx() -- which is essential for the COND_SYSCALL() and SYS_NI()
  * macros to work correctly.
  */
+#ifdef CONFIG_UNIKERNEL_LINUX
+#define SYSCALL_DEFINE0(sname)						\
+	static long __ukl_##sname(const struct pt_regs *__unused);	\
+	static long __ukl_##sname(const struct pt_regs *__unused)
+#else
 #define SYSCALL_DEFINE0(sname)						\
 	SYSCALL_METADATA(_##sname, 0);					\
 	static long __do_sys_##sname(const struct pt_regs *__unused);	\
 	__X64_SYS_STUB0(sname)						\
 	__IA32_SYS_STUB0(sname)						\
 	static long __do_sys_##sname(const struct pt_regs *__unused)
+#endif
 
 #define COND_SYSCALL(name)						\
 	__X64_COND_SYSCALL(name)					\
