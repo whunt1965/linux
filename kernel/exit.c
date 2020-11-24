3d66b7c0276fde351f0e370ef0f625d365b7e6ae
Ali Raza <aliraza@bu.edu>
2020-11-24 02:45:07 -0500

    Now tlb flushes occur if mm is init_mm, they werent happening earlier. Also, fixing exit code so UKL doesnt exit and barf

--- kernel/exit.c
+++ kernel/exit.c
@@ -761,9 +761,16 @@ void __noreturn do_exit(long code)
 		 * If the last thread of global init has exited, panic
 		 * immediately to get a useable coredump.
 		 */
-		if (unlikely(is_global_init(tsk)))
+		if (unlikely(is_global_init(tsk))){
+			printk("UKL exiting\n");
+			while(1){
+				current->state = TASK_INTERRUPTIBLE;
+				schedule();
+			}
 			panic("Attempted to kill init! exitcode=0x%08x\n",
 				tsk->signal->group_exit_code ?: (int)code);
+		}
+
 
 #ifdef CONFIG_POSIX_TIMERS
 		hrtimer_cancel(&tsk->signal->real_timer);
