ec02401a6f7a35ef89758ab8ca8f369df40ccc14
Ali Raza <aliraza@bu.edu>
2020-12-03 13:23:00 -0500

    adding changes so that in stack switch bypass case, we change the stack and run ukl specific entry exit code

--- arch/x86/include/asm/syscall_wrapper.h
+++ arch/x86/include/asm/syscall_wrapper.h
@@ -3,6 +3,8 @@
  * syscall_wrapper.h - x86 specific wrappers to syscall definitions
  */
 
+#include <asm/percpu.h>
+
 #ifndef _ASM_X86_SYSCALL_WRAPPER_H
 #define _ASM_X86_SYSCALL_WRAPPER_H
 
@@ -224,6 +226,10 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
 
 #endif /* CONFIG_COMPAT */
 
+extern void ukl_ss_u2k(void);
+extern void ukl_ss_k2u(void);
+
+#ifdef CONFIG_UKL_SAME_STACK
 #define __SYSCALL_DEFINEx(x, name, ...)					\
 	static long __se_sys##name(__MAP(x,__SC_LONG,__VA_ARGS__));	\
 	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__));\
@@ -241,6 +247,29 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
 		return ret;						\
 	}								\
 	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__))
+#else
+#define __SYSCALL_DEFINEx(x, name, ...)					\
+	static long __se_sys##name(__MAP(x,__SC_LONG,__VA_ARGS__));	\
+	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__));\
+	__X64_SYS_STUBx(x, name, __VA_ARGS__)				\
+	__IA32_SYS_STUBx(x, name, __VA_ARGS__)				\
+	long __ukl##name(__MAP(x,__SC_DECL,__VA_ARGS__))		\
+	{								\
+		long ret;						\
+		ukl_ss_u2k();						\
+		ret = __do_sys##name(__MAP(x,__SC_CAST,__VA_ARGS__));	\
+		ukl_ss_k2u();						\
+		return ret;						\
+	}								\
+	static long __se_sys##name(__MAP(x,__SC_LONG,__VA_ARGS__))	\
+	{								\
+		long ret = __do_sys##name(__MAP(x,__SC_CAST,__VA_ARGS__));\
+		__MAP(x,__SC_TEST,__VA_ARGS__);				\
+		__PROTECT(x, ret,__MAP(x,__SC_ARGS,__VA_ARGS__));	\
+		return ret;						\
+	}								\
+	static inline long __do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__))
+#endif
 
 /*
  * As the generic SYSCALL_DEFINE0() macro does not decode any parameters for
@@ -249,6 +278,7 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
  * SYSCALL_DEFINEx() -- which is essential for the COND_SYSCALL() and SYS_NI()
  * macros to work correctly.
  */
+#ifdef CONFIG_UKL_SAME_STACK
 #define SYSCALL_DEFINE0(sname)						\
 	SYSCALL_METADATA(_##sname, 0);					\
 	static long __do_sys_##sname(const struct pt_regs *__unused);	\
@@ -259,6 +289,22 @@ extern long __ia32_sys_ni_syscall(const struct pt_regs *regs);
 		return __do_sys_##sname(NULL);				\
 	}								\
 	static long __do_sys_##sname(const struct pt_regs *__unused)
+#else
+#define SYSCALL_DEFINE0(sname)						\
+	SYSCALL_METADATA(_##sname, 0);					\
+	static long __do_sys_##sname(const struct pt_regs *__unused);	\
+	__X64_SYS_STUB0(sname)						\
+	__IA32_SYS_STUB0(sname)						\
+	long __ukl_##sname(void)					\
+	{								\
+		long ret;						\
+		ukl_ss_u2k();						\
+		ret = __do_sys_##sname(NULL);				\
+		ukl_ss_k2u();						\
+		return ret;						\
+	}								\
+	static long __do_sys_##sname(const struct pt_regs *__unused)
+#endif
 
 #define COND_SYSCALL(name)						\
 	__X64_COND_SYSCALL(name)					\
