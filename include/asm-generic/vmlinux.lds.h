ebf09894c4d95b5dce61dc395f8340b59b48cf19
Ali Raza <aliraza@bu.edu>
2021-04-30 10:34:08 -0400

    uklnormal 1. Seemingly ok changes

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -548,14 +548,24 @@
  * code elimination is enabled, so these sections should be converted
  * to use ".." first.
  */
+#ifdef CONFIG_UNIKERNEL_LINUX
+#define TEXT_TEXT							\
+		ALIGN_FUNCTION();					\
+		*(.text.hot .text.hot.* TEXT_MAIN .stub .text.* .gnu.linkonce.t.* .text.fixup .text.unlikely .text.*_unlikely .text.unlikely.*)	\
+		*(.text.exit .text.exit.*) *(.text.startup .text.startup.*)	\
+		*(.text..refcount)					\
+		*(.ref.text)						\
+	MEM_KEEP(init.text*)						\
+	MEM_KEEP(exit.text*)
+#else
 #define TEXT_TEXT							\
 		ALIGN_FUNCTION();					\
 		*(.text.hot TEXT_MAIN .text.fixup .text.unlikely)	\
 		*(.text..refcount)					\
 		*(.ref.text)						\
 	MEM_KEEP(init.text*)						\
-	MEM_KEEP(exit.text*)						\
-
+	MEM_KEEP(exit.text*)
+#endif
 
 /* sched.text is aling to function alignment to secure we have same
  * address even at second ld pass when generating System.map */
@@ -917,6 +927,18 @@
 	EXIT_DATA
 #endif
 
+#ifdef CONFIG_UNIKERNEL_LINUX
+#define DISCARDS							\
+	/DISCARD/ : {							\
+	EXIT_DISCARDS							\
+	EXIT_CALL							\
+	*(.discard)							\
+	*(.discard.*)							\
+	*(.modinfo)							\
+	*(.gnu.glibc-stub.*)						\
+	*(.gnu.warning.*)						\
+	}
+#else
 #define DISCARDS							\
 	/DISCARD/ : {							\
 	EXIT_DISCARDS							\
@@ -925,6 +947,7 @@
 	*(.discard.*)							\
 	*(.modinfo)							\
 	}
+#endif
 
 /**
  * PERCPU_INPUT - the percpu input sections
